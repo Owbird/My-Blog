5fd56f841a0da65e02bc5d52a8952e5b5797e327
owbird <owbirdphorccuo@gmail.com>
2024-11-08 06:32:00 +0000

    chore: Insert G anal

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          {process.env.NODE_ENV !== 'development' && (
+            <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          )}
           <SectionContainer>
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
